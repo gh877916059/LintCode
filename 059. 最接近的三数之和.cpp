ff41892b1a9a7d88d594aff2a1bda5748db2eacf
卢伟荣 <877916059@qq.com>
2017-09-08 11:45:16 +0800

    Create 059. 最接近的三数之和.cpp

---
+++ 059. 最接近的三数之和.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int closest = nums[0] + nums[1] + nums[2];
+        int diff = abs(closest - target);
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            int left = i + 1, right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                int newDiff = abs(sum - target);
+                if (diff > newDiff) {
+                    diff = newDiff;
+                    closest = sum;
+                }
+                if (sum < target) ++left;
+                else --right;
+            }
+        }
+        return closest;
+    }
+};
