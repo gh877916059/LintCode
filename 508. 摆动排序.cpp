25b95341ea899e322412c4671316ab8269c0a618
卢伟荣 <877916059@qq.com>
2017-09-08 13:43:57 +0800

    Create 508. 摆动排序.cpp

---
+++ 508. 摆动排序.cpp
@@ -0,0 +1,13 @@
+class Solution 
+{
+public:
+    void wiggleSort(vector<int> &nums) 
+    {
+        if (nums.size() <= 1) return;
+        for (int i = 1; i < nums.size(); ++i) 
+        {
+            if ((i % 2 == 1 && nums[i] < nums[i - 1]) || (i % 2 == 0 && nums[i] > nums[i - 1])) 
+                swap(nums[i], nums[i - 1]);
+        }
+    }
+};
