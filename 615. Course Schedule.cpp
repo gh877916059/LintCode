e05f2302a215b6173889572ff9f52b4b84f1fdbc
卢伟荣 <877916059@qq.com>
2017-09-08 14:07:09 +0800

    Create 615. Course Schedule.cpp

---
+++ 615. Course Schedule.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+class Solution 
+{
+public:
+	bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites)
+	{
+		vector<int> ru_du_list(numCourses, 0);
+		vector<vector<int>> after_can_learn_list(numCourses, vector<int>());
+		for (auto itr = prerequisites.begin(); itr != prerequisites.end(); ++itr)
+		{
+			//a依赖于b
+			int a = (*itr).first;
+			int b = (*itr).second;
+			ru_du_list[a] += 1;
+			after_can_learn_list[b].push_back(a);
+		}
+		vector<int> ret;
+		vector<int> curr_list;
+		for (int i = ru_du_list.size() - 1; i >= 0; --i)
+		{
+			if (ru_du_list[i] == 0)
+			{
+				curr_list.push_back(i);
+			}
+		}
+		while (curr_list.size()>0)
+		{
+			int curr_chosen = curr_list[curr_list.size() - 1];
+			curr_list.pop_back();
+			ret.push_back(curr_chosen);
+			for (int j = after_can_learn_list[curr_chosen].size() - 1; j >= 0; --j)
+			{
+				int target = after_can_learn_list[curr_chosen][j];
+				ru_du_list[target] -= 1;
+				if (ru_du_list[target] == 0)
+				{
+					curr_list.push_back(target);
+				}
+			}
+		}
+		if (ret.size() < numCourses)
+			return false;
+		return true;
+	}
+};
