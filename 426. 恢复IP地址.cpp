b86e5047f4149be113bd3d9ae9ff050bf35b4573
卢伟荣 <877916059@qq.com>
2017-09-08 13:42:13 +0800

    Create 426. 恢复IP地址.cpp

---
+++ 426. 恢复IP地址.cpp
@@ -0,0 +1,36 @@
+class Solution 
+{
+public:
+    vector<string> restoreIpAddresses(string s) 
+    {
+        vector<string> result;
+        string ip;
+        dfs(s, 0, 0, ip, result);
+        return result;
+    }
+    void dfs(string s, int start, int step, string ip,vector<string> &result) 
+    {
+        if (s.size() - start > (4 - step) * 3)
+            return; 
+        if (s.size() - start < (4 - step))
+            return; 
+        if (start == s.size() && step == 4) 
+        { 
+            ip.resize(ip.size() - 1);
+            result.push_back(ip);
+            return;
+        }
+        int num = 0;
+        for (int i = start; i < start + 3; i++) 
+        {
+            num = num * 10 + (s[i] - '0');
+            if (num <= 255) 
+            { 
+                ip += s[i];
+                dfs(s, i + 1, step + 1, ip + '.', result);
+            }
+            if (num == 0) 
+                break; 
+        }
+    }
+};
