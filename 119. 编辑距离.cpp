832becc814c1daa9dbf2f1f178f762004047e199
卢伟荣 <877916059@qq.com>
2017-09-08 12:36:02 +0800

    Create 119. 编辑距离.cpp

---
+++ 119. 编辑距离.cpp
@@ -0,0 +1,26 @@
+class Solution {  
+public:      
+    int minDistance(string word1, string word2) 
+    {  
+        int n = word1.length();  
+        int m = word2.length();  
+        vector<vector<int> > dp(n+1,vector<int>(m+1,0));  
+        for(int i = 1;i<=n;i++)  
+            dp[i][0]=i;  
+        for(int i=1;i<=m;i++)  
+            dp[0][i]=i;  
+        for(int i=1;i<=n;i++)  
+        {  
+            char c1 = word1[i-1];  
+            for(int j=1;j<=m;j++)  
+            {  
+                char c2 = word2[j-1];  
+                if(c1==c2)  
+                    dp[i][j] = dp[i-1][j-1];  
+                else  
+                    dp[i][j] = min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]))+1;  
+            }  
+        }  
+        return dp[n][m];  
+    }  
+}; 
